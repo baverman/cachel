156d3230a2b7fd3416715aa5706473900adbe708
Anton Bobrov <baverman@gmail.com>
2019-06-24 11:08:14 +0300

    release 0.9.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='cachel',
-    version='0.8.0',
+    version='0.9.0',
     url='https://github.com/baverman/cachel/',
     license='MIT',
     author='Anton Bobrov',
